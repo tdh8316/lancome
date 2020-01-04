c212861bd49bee9a5a464a5012eb14c1934c1766
탁동혁 <tdh8316@naver.com>
2020-01-04 12:27:25 +0900

    Update setup tools

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 import lancome
 
 setup(
     name='lancome',
     version=lancome.__version__,
-    packages=[''],
+    packages=find_packages(exclude=['docs', 'tests*']),
     url='https://github.com/tdh8316/lancome',
     license='GNU General Public License v3.0',
     author='Donghyeok Tak',
     author_email='tdh8316@naver.com',
-    description=open("./README.md", 'r', encoding="UTF-8").readlines()[1][:-2],
-    install_requires=['colorama']
+    description=open('./README.md', 'r', encoding='UTF-8').readlines()[1][:-2],
+    long_description=open('README.md', 'r', encoding='UTF-8').read(),
+    install_requires=['colorama'],
+    python_requires='>=3',
 )
